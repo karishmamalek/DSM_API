56b7ee4ab9040ad8362c6e6aaedf76e9e3b4c4eb
karishmamalek <karimalik26997@gmail.com>
2022-11-18 19:18:04 +0530

    API Configuration

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,11 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
 
 class Category extends Model
 {
     use HasFactory;
+    
+    /*private $TBL_CATEGORY='';
+    public function __construct()
+    {
+        $this->TBL_CATEGORY = Config::get('constants.TBL_CATEGORY');
+        $table = $this->TBL_CATEGORY;
+    }*/
+    
     protected $table = 'category';
 
     public static function getcat_android_app_version($category_id){
